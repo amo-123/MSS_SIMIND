ab3fb8d71694311da46fbccbfbe0bffbd82c3350
Ashley <a.j.morahan@gmail.com>
2019-02-11 14:42:32 +0000

    Line Source Bash Script
    Test script for line source

---
+++ MSS/MSS_LineSource_500Count.sh
@@ -0,0 +1,21 @@
+# source centred at Double slit
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-10./26:1000/29:1 mss_line_twoslit_500count1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-7.5/26:1000/29:1 mss_line_twoslit_500count2
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-5.0/26:1000/29:1 mss_line_twoslit_milcount3%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-2.5/26:1000/29:1 mss_line_twoslit_milcount4%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:0.00/26:1000/29:1 mss_line_twoslit_milcount5%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:2.50/26:1000/29:1 mss_line_twoslit_milcount6%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:5.00/26:1000/29:1 mss_line_twoslit_milcount7%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:7.50/26:1000/29:1 mss_line_twoslit_milcount8%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:10.0/26:1000/29:1 mss_line_twoslit_milcount9%1
+
+# source centres at Triple slits
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-10./26:1000/29:1 mss_line_milcount1a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-7.5/26:1000/29:1 mss_line_milcount2a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-5.0/26:1000/29:1 mss_line_milcount3a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-2.5/26:1000/29:1 mss_line_milcount4a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:0.00/26:1000/29:1 mss_line_milcount5a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:2.50/26:1000/29:1 mss_line_milcount6a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:5.00/26:1000/29:1 mss_line_milcount7a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:7.50/26:1000/29:1 mss_line_milcount8a%1
+# simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:10.0/26:1000/29:1 mss_line_milcount9a%1
