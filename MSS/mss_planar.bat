278efccdfc0866e1608dd53fea630ff7abab9d00
Ashley <a.j.morahan@gmail.com>
2019-02-11 08:22:40 +0000

    MSS data files and script
    These files contain the geometry for the MSS and the batch scripts to run the simulations with MSS

---
+++ MSS/mss_planar.bat
@@ -0,0 +1,39 @@
+
+rem simind slitslat/26:80/x6:w slitslat_a%1 
+rem simind slitslat/26:80/x6:w/23:0 slitslat_b%1 
+
+rem simind slitslat/26:80/x6:w/14:2/15:2/fa:5/fa:11/02:5/03:4/04:0.001/tr:5/29:1/12:1/26:1000 slitslat_plan slitslat.cor
+
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:2.5/26:250  slitslat_line1 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:5.0/26:250  slitslat_line2 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:7.5/26:250  slitslat_line3 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:10.0/26:250 slitslat_line4 slitslat.cor%1
+
+rem simind slitslat/26:80/x6:w/14:4/15:7/fz:cbf/tr:5/if:bar/26:200 slitslat_bar slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:7/fz:cbf/tr:5/if:jaszak/26:200 slitslat_sphere slitslat.cor%1
+
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-10./26:1000/29:1 test_fileoutput%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-7.5/26:1000/29:1 mss_line_twoslit_milcount2%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-5.0/26:1000/29:1 mss_line_twoslit_milcount3%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-2.5/26:1000/29:1 mss_line_twoslit_milcount4%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:0.00/26:1000/29:1 mss_line_twoslit_milcount5%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:2.50/26:1000/29:1 mss_line_twoslit_milcount6%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:5.00/26:1000/29:1 mss_line_twoslit_milcount7%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:7.50/26:1000/29:1 mss_line_twoslit_milcount8%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:10.0/26:1000/29:1 mss_line_twoslit_milcount9%1
+
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-10./26:1000/29:1 mss_line_milcount1a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-7.5/26:1000/29:1 mss_line_milcount2a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-5.0/26:1000/29:1 mss_line_milcount3a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-2.5/26:1000/29:1 mss_line_milcount4a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:0.00/26:1000/29:1 mss_line_milcount5a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:2.50/26:1000/29:1 mss_line_milcount6a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:5.00/26:1000/29:1 mss_line_milcount7a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:7.50/26:1000/29:1 mss_line_milcount8a%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:10.0/26:1000/29:1 mss_line_milcount9a%1
+
+rem simind slitslat/x6:w/14:2/15:2/tr:5/03:5/02:10/04:0.001/16:0.6/18:10.0/26:250/29:2 slitslat_planar_2Proj%1
+rem simind slitslat/x6:w/14:2/15:2/tr:5/03:5/02:10/04:0.001/16:0.6/18:10.0/26:250/29:10 slitslat_planar_10Proj%1
+
+rem simind slitslat/x6:w/14:4/15:5/tr:5/16:0.6/26:250/29:1/ slitslat_pointsource_off_centre%1
+
---
+++ MSS/slitslat.bat
@@ -0,0 +1,33 @@
+
+rem simind slitslat/26:80/x6:w slitslat_a%1 
+rem simind slitslat/26:80/x6:w/23:0 slitslat_b%1 
+
+rem simind slitslat/26:80/x6:w/14:2/15:2/fa:5/fa:11/02:5/03:4/04:0.001/tr:5/29:1/12:1/26:1000 slitslat_plan slitslat.cor
+
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:2.5/26:250  slitslat_line1 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:5.0/26:250  slitslat_line2 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:7.5/26:250  slitslat_line3 slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:4/tr:5/02:5/03:0.001/04:0.001/18:10.0/26:250 slitslat_line4 slitslat.cor%1
+
+rem simind slitslat/26:80/x6:w/14:4/15:7/fz:cbf/tr:5/if:bar/26:200 slitslat_bar slitslat.cor%1
+rem simind slitslat/26:80/x6:w/14:4/15:7/fz:cbf/tr:5/if:jaszak/26:200 slitslat_sphere slitslat.cor%1
+
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-10./26:250/29:1 slitslat_line1b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-7.5/26:250/29:1 slitslat_line2b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-5.0/26:250/29:1 slitslat_line3b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:-2.5/26:250/29:1 slitslat_line4b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:0.00/26:250/29:1 slitslat_line5b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:2.50/26:250/29:1 slitslat_line6b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:5.00/26:250/29:1 slitslat_line7b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:7.50/26:250/29:1 slitslat_line8b%1
+rem simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/18:10.0/26:250/29:1 slitslat_line9b%1
+
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-10./26:250/29:1 slitslat_line1c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-7.5/26:250/29:1 slitslat_line2c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-5.0/26:250/29:1 slitslat_line3c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:-2.5/26:250/29:1 slitslat_line4c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:0.00/26:250/29:1 slitslat_line5c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:2.50/26:250/29:1 slitslat_line6c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:5.00/26:250/29:1 slitslat_line7c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:7.50/26:250/29:1 slitslat_line8c%1
+simind slitslat/26:80/x6:w/14:4/15:2/tr:5/03:5/02:0.001/04:0.001/16:0.6/18:10.0/26:250/29:1 slitslat_line9c%1
